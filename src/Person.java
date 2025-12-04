032703e563dc4d3f0d23f91acd3b97144e86b9aa
Artem <jock.fromrussia@gmail.com>
2025-12-04 12:35:03 +0300

    добавил файл person.java

---
+++ src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    public String name;
+    public int age;
+    // максон пофиксить!!!
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
